7979d32e85a8f2eb85d15d389bac0f68e03605f1
Rafael <rosselgalarzarafael@gmail.com>
2024-11-06 18:52:46 -0500

    añadiendo update

--- src/modules/task/task.controller.js
+++ src/modules/task/task.controller.js
@@ -23,33 +23,58 @@ const createTasks = async (req, res) => {
 }
 
 
-const updateTasks = (req, res) => {
-
+const updateTasks = async (req, res) => {
+    const { title, description } = req.body;
+    const [result] = await pool.query("select * from tasks where id = ? ", [req.body, req.params.id])
+    if (result.length == 0) {
+        return res.status(404).json({ message: "task no foud" })
+    } else {
+        pool.query("update tasks set title = ? , set description = ? where id=?")
+    }
+    res.json({
+        result
+    })
 }
+const updateTasks1 = async (req, res) => {
+    const { title, description } = req.body
+    const {id} = req.params
+    const [result] = await pool.query("UPDATE tasks SET ? WHERE id= ?", [req.body, req.params.id])
+    if (result.affectedRows===0 ){
+        return res.status(404).json({message: "task no found"})
+    }
+    res.json({
+        id,
+        title,
+        description
+
+    }
+    )
+}
+
 
 
 const getIdTasks = async (req, res) => {
     const [result] = await pool.query("select * from tasks where id = ? ", [req.params.id])
-    if (result.length==0){
-      
-        return  res.status(404).json({message: "task no found"})
+    if (result.length == 0) {
+
+        return res.status(404).json({ message: "task no found" })
     }
-    
+
     res.json(result[0])
 }
 
 
 const deleteTasks = async (req, res) => {
-const [result] = await pool.query("delete from tasks where id= ?",[req.params.id] )
-if (result.affectedRows ===0){
-      
-    return  res.status(404).json({message: "task no found"})
-}
+    const [result] = await pool.query("delete from tasks where id= ?", [req.params.id])
+    if (result.affectedRows === 0) {
 
-return res.sendStatus(204)
+        return res.status(404).json({ message: "task no found" })
+    }
+
+    return res.sendStatus(204)
 
 }
 
 export {
-    getTasks, createTasks, getIdTasks, deleteTasks
+    getTasks, createTasks, getIdTasks, deleteTasks, updateTasks, updateTasks1
 }
--- src/modules/task/task.routes.js
+++ src/modules/task/task.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getTasks , createTasks, getIdTasks, deleteTasks} from './task.controller.js'
+import { getTasks , createTasks, getIdTasks, deleteTasks, updateTasks1} from './task.controller.js'
 
 const router = Router();
 
 router.get('/tasks', getTasks)
 router.post('/tasks', createTasks)
-router.put('/tasks/:id')
+router.put('/tasks/:id', updateTasks1)
 router.get('/tasks/:id', getIdTasks)
 router.delete('/tasks/:id', deleteTasks)
 
