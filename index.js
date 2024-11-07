9f52ee5552c1cc9f2eef644f526524873730bd07
Rafael <rosselgalarzarafael@gmail.com>
2024-11-06 21:17:14 -0500

    añadiendo cors

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import  taskRoutes from './src/modules/task/task.routes.js'
+import cors from 'cors'
 const app = express();
 const port = 3000;
-
 
 // una forma corta, una funcion anonima con felcha
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
+app.use(cors())
 app.use(express.json())
 
 app.use(taskRoutes);
